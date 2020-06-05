d51a6ffa54026c7411f112f26e0787133e86886e
Sanidhya-Sinha <64468053+Sanidhya-Sinha@users.noreply.github.com>
2020-06-05 18:28:28 +0530

    Add files via upload

--- Data_collection.py
+++ Data_collection.py
@@ -215,13 +215,3 @@ while True:
     
 cap.release()
 cv2.destroyAllWindows()
-"""
-d = "old-data/test/0"
-newd = "data/test/0"
-for walk in os.walk(d):
-    for file in walk[2]:
-        roi = cv2.imread(d+"/"+file)
-        roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        _, mask = cv2.threshold(roi, 120, 255, cv2.THRESH_BINARY)
-        cv2.imwrite(newd+"/"+file, mask)     
-"""
